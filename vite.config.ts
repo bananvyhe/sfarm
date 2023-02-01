cb50288979874ca2073ba2a6c47082b7c0305193
bananvyhe <loadonden@yahoo.com>
2023-02-02 01:50:18 +0500

    element-plus install ui

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,23 @@ import { defineConfig } from 'vite'
 import RubyPlugin from 'vite-plugin-ruby'
 import vue from '@vitejs/plugin-vue'
 
+import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite'
+import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
+
 export default defineConfig({
   plugins: [
     RubyPlugin(),
-    vue()
+    vue(),
+     AutoImport({
+      resolvers: [ElementPlusResolver()],
+    }),
+    Components({
+      resolvers: [ElementPlusResolver()],
+    }),
   ],
 })
+
+
+
+ 
